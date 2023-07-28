32a6f7ffbd02d4fed1956e753e2ea85ada4d6c2c
sevro49 <21120205303@ismu.edu.tr>
2023-07-28 19:12:09 +0300

    v-for for list

--- app.js
+++ app.js
@@ -2,29 +2,17 @@ const app = Vue.createApp({
     data() {
         return {
             isVisible: true,
-            title: "The Final Empire",
-            author: "Brandon Sanderson",
-            age: 45,
-            x: 0,
-            y: 0,
+            books: [
+                { title: "name of the wind", author: "patrik rothfuss" },
+                { title: "the way of kings", author: "brandon sanderson" },
+                { title: "the final empire", author: "brandon sanderson" },
+            ],
         };
     },
 
     methods: {
         toggleShowBooks() {
             this.isVisible = !this.isVisible;
-        },
-
-        handleEvent(e, data) {
-            console.log(e, e.type);
-            if (data) {
-                console.log(data);
-            }
-        },
-
-        handleMouseMove(e) {
-            this.x = e.offsetX;
-            this.y = e.offsetY;
         },
     },
 });
