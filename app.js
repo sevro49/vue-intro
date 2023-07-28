e3406852454ef5aad4fb0fdc83f2606a519530c5
sevro49 <21120205303@ismu.edu.tr>
2023-07-28 20:16:57 +0300

    computed properties

--- app.js
+++ app.js
@@ -32,7 +32,13 @@ const app = Vue.createApp({
         },
 
         toggleFav(book) {
-            book.isFav = !book.isFav
+            book.isFav = !book.isFav;
+        },
+    },
+
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav);
         },
     },
 });
