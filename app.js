9857f3f3964b50f302ed2fff4d2811c555142050
sevro49 <21120205303@ismu.edu.tr>
2023-07-28 16:15:33 +0300

    mouseover, mouseleave, dblclick, mousemove

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ const app = Vue.createApp({
             title: "The Final Empire",
             author: "Brandon Sanderson",
             age: 45,
+            x: 0,
+            y: 0,
         };
     },
 
     methods: {
-        toggleShowBooks(){
+        toggleShowBooks() {
             this.isVisible = !this.isVisible;
+        },
+
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if (data) {
+                console.log(data);
+            }
+        },
+
+        handleMouseMove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         },
     },
 });
