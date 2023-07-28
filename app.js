d5fe4e5cd1908610c0d5d6769aebbfe2d6bb41e1
sevro49 <21120205303@ismu.edu.tr>
2023-07-28 19:30:16 +0300

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: "https://www.youtube.com",
             isVisible: true,
             books: [
-                { title: "name of the wind", author: "patrik rothfuss" },
-                { title: "the way of kings", author: "brandon sanderson" },
-                { title: "the final empire", author: "brandon sanderson" },
+                {
+                    title: "name of the wind",
+                    author: "patrik rothfuss",
+                    img: "assets/1.jpg",
+                },
+                {
+                    title: "the way of kings",
+                    author: "brandon sanderson",
+                    img: "assets/2.jpeg",
+                },
+                {
+                    title: "the final empire",
+                    author: "brandon sanderson",
+                    img: "assets/3.jpeg",
+                },
             ],
         };
     },
