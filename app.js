77b08f5f7bf15a709bbdf71da683cb3f955b6d5e
sevro49 <21120205303@ismu.edu.tr>
2023-07-28 19:45:32 +0300

    dynamic classes

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const app = Vue.createApp({
                     title: "name of the wind",
                     author: "patrik rothfuss",
                     img: "assets/1.jpg",
+                    isFav: true,
                 },
                 {
                     title: "the way of kings",
                     author: "brandon sanderson",
                     img: "assets/2.jpeg",
+                    isFav: false,
                 },
                 {
                     title: "the final empire",
                     author: "brandon sanderson",
                     img: "assets/3.jpeg",
+                    isFav: true,
                 },
             ],
         };
