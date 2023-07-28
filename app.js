9d3ec5de9be32538394e1dde40ea14a11a7c784e
sevro49 <21120205303@ismu.edu.tr>
2023-07-28 15:28:25 +0300

    click events

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = Vue.createApp({
             age: 45,
         };
     },
+
+    methods: {
+        changeTitle(title) {
+            // this.title = "Words of Randiance";
+            this.title = title;
+        },
+    },
 });
 
 app.mount("#app");
