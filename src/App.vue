3cc525c98069977ba1cd5c80385f104569b3bb1b
sevro49 <21120205303@ismu.edu.tr>
2023-07-29 16:27:48 +0300

    props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>{{ title }}</h1>
-    <Modal />
+    <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
@@ -14,6 +14,8 @@ export default {
     data() {
         return {
             title: "My first Vue app",
+            header: "Sign up for the Giveaway!",
+            text: "Grab your ninja swag for half price!",
         };
     },
 };
