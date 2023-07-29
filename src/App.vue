56c15fe9c39ee174faec1ded54744f6cd663a9d8
sevro49 <21120205303@ismu.edu.tr>
2023-07-29 17:12:07 +0300

    slots

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
     <h1>{{ title }}</h1>
     <p>Welcome...</p>
     <div v-if="showModal">
-        <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+        <Modal theme="sale" @close="toggleModal">
+            <template v-slot:links>
+            <a href="#">Sign up now</a>
+            <a href="#">Info</a>
+            </template>
+            <h1>Ninja Giveaway!</h1>
+            <p>Grab your ninja swag for half price!</p>
+        </Modal>
     </div>
     <button @click="toggleModal">open modal</button>
 </template>
